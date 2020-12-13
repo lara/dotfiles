6e659dd2ab9289a333414262567f1052f0a6c264
Lara Aydin <ayd.dilara@gmail.com>
2020-12-13 12:33:42 -0800

    some aliases

---
+++ aliases.zsh
@@ -0,0 +1,22 @@
+# PS
+alias psa="ps aux"
+alias psg="ps aux | grep "
+alias psr='ps aux | grep ruby'
+
+# Moving around
+alias cdb='cd -'
+alias cls='clear;ls'
+
+alias df='df -h'
+alias du='du -h -d 2'
+alias ll='ls -alGh'
+alias ls='ls -Gh'
+alias lsg='ll | grep'
+alias ka9='killall -9'
+alias k9='kill -9'
+
+# Gem install
+alias sgi='sudo gem install --no-ri --no-rdoc'
+
+# Homebrew
+alias bru='brew update && brew upgrade && brew cleanup && brew doctor'
